2d6b7f2fdda4a6aee34bc2493fb0341fbd617d45
ytytzz <yeontaezz22@gmail.com>
2025-12-05 21:16:48 +0900

    add: router추가

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ListPage from "./page/ListPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<div>메인페이지</div>}></Route>
+        <Route path="/list" element={<ListPage />}></Route>
+      </Routes>
     </div>
   );
 }
