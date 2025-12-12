9c4d1373584595f7b7b9d66c11885af0381795dc
YunDongGeun <dong@geun.kr>
2025-12-13 03:52:28 +0900

    feat: 관리자 페이지 구현

--- app/src/App.js
+++ app/src/App.js
@@ -5,24 +5,23 @@ import ListPage from "./page/ListPage";
 import MainPage from "./page/MainPage"
 import ProductDetailPage from './page/ProductDetailPage';
 import OrderHistoryPage from './page/OrderHistoryPage';
-import LoginPage from './page/LoginPage'; // LoginPage 임포트
+import LoginPage from './page/LoginPage';
+import AdminPage from './page/AdminPage';
 
 function App() {
   return (
     <div style={{ minHeight: "100vh", background: "#fafafa" }}>
       <Header />
-      <main style={{ maxWidth: 1100, margin: "0 auto", padding: "24px" }}/>
+      <main style={{ maxWidth: 1100, margin: "0 auto", padding: "24px" }}>
         <Routes>
-    <Route path="/" element={<MainPage />} />
-          <Route path="/" element={<Navigate to="/list" replace />} />
-           <Route path="/products/:id" element={<ProductDetailPage />}/>
-
-       
+          <Route path="/" element={<MainPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
           <Route path="/list" element={<ListPage />} />
-        <Route path="/order-history" element={<OrderHistoryPage />} />
-          <Route path="/account/login" element={<LoginPage />} /> {/* 로그인 페이지 라우트 추가 */}
+          <Route path="/order-history" element={<OrderHistoryPage />} />
+          <Route path="/account/login" element={<LoginPage />} />
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
-     
+      </main>
     </div>
   );
 }
